be97dba0e07296ac6ca990f81e46d6bfb36a13cb
Sreejith R <rsreejith090@gmail.com>
2025-08-27 21:01:16 +0530

    changed to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import {  HashRouter } from "react-router-dom";
+import {  BrowserRouter } from "react-router-dom";
 import { Provider } from "./components/ui/provider";
-
+BrowserRouter
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Provider>
         <App />
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
