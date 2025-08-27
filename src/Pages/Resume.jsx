ae2785379bfb4077b79035708c584a1b42ce1c6a
Sreejith R <rsreejith090@gmail.com>
2025-08-27 20:52:35 +0530

    BrowserRouter changed to HashRouter

---
+++ src/Pages/Resume.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Resume = () => {
+  return (
+    <div>
+        <div className='container'>
+            <h1>hello</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Resume
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 import { Provider } from "./components/ui/provider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
