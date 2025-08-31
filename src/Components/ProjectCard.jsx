5be5a764d98d4cfc932ffd4073521019f8c5f6f2
Sreejith R <rsreejith090@gmail.com>
2025-08-31 20:48:53 +0530

    making project component responsive

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -9,6 +9,7 @@ import Ecart from "../assets/E-cart.png";
 import nexgen from "../assets/nexgen.png";
 import zomato from "../assets/zomato.png";
 import trends from "../assets/trends.png";
+
 const ProjectCard = () => {
   useEffect(() => {
     Aos.init({
@@ -16,116 +17,144 @@ const ProjectCard = () => {
       once: false,
     });
   }, []);
+  
   return (
-    <div className="container projects  " id="projects">
-      <h1
-        style={{ color: "aqua" }}
-        className="text-center mt-3 mb-5"
-        data-aos="fade-right"
-      >
-        My Latest Projects
-      </h1>
-      <div className="row mb-4  " data-aos="fade-right">
-        {/* first col */}
-        <div className="col">
-          <a href="https://github.com/Sreejith0098/Media-Player-.git">
-            <Card.Root maxW="sm" overflow="hidden">
-              <Image src={media} alt="Green double couch with wooden legs" />
-              <Card.Body gap="2" className="text-center">
-                <Card.Title>Media Player </Card.Title>
-                <Card.Description>
-                  Media player using
-                  React,html,css,jsvascript,Bootswatch,ReactBootstrap
-                </Card.Description>
-              </Card.Body>
-              <Card.Footer gap="2"></Card.Footer>
-            </Card.Root>
-          </a>
-        </div>
+    <div className="container-fluid projects" id="projects">
+      <div className="container">
+        <h1
+          style={{ color: "aqua" }}
+          className="text-center mt-3 mb-5"
+          data-aos="fade-right"
+        >
+          My Latest Projects
+        </h1>
+        
+        <div className="row mb-4" data-aos="fade-right">
+          {/* first col */}
+          <div className="col-12 col-md-6 col-lg-4 mb-4">
+            <a href="https://github.com/Sreejith0098/Media-Player-.git">
+              <Card.Root overflow="hidden" className="h-100">
+                <Image 
+                  src={media} 
+                  alt="Media Player App" 
+                  className="img-fluid"
+                />
+                <Card.Body gap="2" className="text-center">
+                  <Card.Title>Media Player</Card.Title>
+                  <Card.Description>
+                    Media player using
+                    React,html,css,jsvascript,Bootswatch,ReactBootstrap
+                  </Card.Description>
+                </Card.Body>
+                <Card.Footer gap="2"></Card.Footer>
+              </Card.Root>
+            </a>
+          </div>
 
-        {/* second col */}
-        <div className="col">
-          <a href="https://github.com/Sreejith0098/RecipeApp-using-React.git">
-            <Card.Root maxW="sm" overflow="hidden">
-              <Image
-                src={RecipeApp}
-                alt="Green double couch with wooden legs"
-              />
-              <Card.Body gap="2" className="text-center">
-                <Card.Title>Recipe App</Card.Title>
-                <Card.Description>
-                  Recipe app using html,css,JavaScript,React,Bootstrap,Api fetch
-                  using async await
-                </Card.Description>
-              </Card.Body>
-              <Card.Footer gap="2"></Card.Footer>
-            </Card.Root>
-          </a>
+          {/* second col */}
+          <div className="col-12 col-md-6 col-lg-4 mb-4">
+            <a href="https://github.com/Sreejith0098/RecipeApp-using-React.git">
+              <Card.Root overflow="hidden" className="h-100">
+                <Image
+                  src={RecipeApp}
+                  alt="Recipe App"
+                  className="img-fluid"
+                />
+                <Card.Body gap="2" className="text-center">
+                  <Card.Title>Recipe App</Card.Title>
+                  <Card.Description>
+                    Recipe app using html,css,JavaScript,React,Bootstrap,Api fetch
+                    using async await
+                  </Card.Description>
+                </Card.Body>
+                <Card.Footer gap="2"></Card.Footer>
+              </Card.Root>
+            </a>
+          </div>
+          
+          {/* third col */}
+          <div className="col-12 col-md-6 col-lg-4 mb-4">
+            <a href="https://github.com/Sreejith0098/ReduxCart.git">
+              <Card.Root overflow="hidden" className="h-100">
+                <Image 
+                  src={Ecart} 
+                  alt="E-cart App" 
+                  className="img-fluid"
+                />
+                <Card.Body gap="2" className="text-center">
+                  <Card.Title>E-cart</Card.Title>
+                  <Card.Description>
+                    E-cart using
+                    html,css,JavaScript,React,Redux,Bootstrap,Bootswatch
+                  </Card.Description>
+                </Card.Body>
+                <Card.Footer gap="2"></Card.Footer>
+              </Card.Root>
+            </a>
+          </div>
         </div>
-        {/* third col */}
-        <div className="col">
-          <a href="https://github.com/Sreejith0098/ReduxCart.git">
-            <Card.Root maxW="sm" overflow="hidden">
-              <Image src={Ecart} alt="Green double couch with wooden legs" />
-              <Card.Body gap="2" className="text-center">
-                <Card.Title>E-cart</Card.Title>
-                <Card.Description>
-                  E-cart using
-                  html,css,JavaScript,React,Redux,Bootstrap,Bootswatch
-                </Card.Description>
-              </Card.Body>
-              <Card.Footer gap="2"></Card.Footer>
-            </Card.Root>
-          </a>
-        </div>
-      </div>
-      {/* 2nd row */}
-      <div className="row" data-aos="fade-right">
-        {/* first col */}
-        <div className="col">
-          <a href="https://github.com/bibinb603/NEXGEN.git">
-            <Card.Root maxW="sm" overflow="hidden">
-              <Image src={nexgen} alt="Green double couch with wooden legs" />
-              <Card.Body gap="2" className="text-center">
-                <Card.Title>Gamming Magazine Website</Card.Title>
-                <Card.Description>
-                created using html,css,bootstrap,Gsap
-                  
-                </Card.Description>
-              </Card.Body>
-              <Card.Footer gap="2"></Card.Footer>
-            </Card.Root>
-          </a>
-        </div>
-        {/* second col */}
-        <div className="col">
-          <a href="https://github.com/Sreejith0098/zomato-clone.git">
-            <Card.Root maxW="sm" overflow="hidden">
-              <Image src={zomato} alt="Green double couch with wooden legs" />
-              <Card.Body gap="2" className="text-center">
-                <Card.Title>Zomato Clone</Card.Title>
-                <Card.Description>
-                  Zomato clone using html,css,bootstrap
-                </Card.Description>
-              </Card.Body>
-              <Card.Footer gap="2"></Card.Footer>
-            </Card.Root>
-          </a>
-        </div>
-        {/* third col */}
-        <div className="col">
-          <a href="https://github.com/Sreejith0098/trends2.git">
-            <Card.Root maxW="sm" overflow="hidden">
-              <Image src={trends} alt="Green double couch with wooden legs" />
-              <Card.Body gap="2" className="text-center">
-                <Card.Title>Trends Clone</Card.Title>
-                <Card.Description>
-                  Trends clone using html,css,bootstrap
-                </Card.Description>
-              </Card.Body>
-              <Card.Footer gap="2"></Card.Footer>
-            </Card.Root>
-          </a>
+        
+        {/* 2nd row */}
+        <div className="row" data-aos="fade-right">
+          {/* first col */}
+          <div className="col-12 col-md-6 col-lg-4 mb-4">
+            <a href="https://github.com/bibinb603/NEXGEN.git">
+              <Card.Root overflow="hidden" className="h-100">
+                <Image 
+                  src={nexgen} 
+                  alt="Gaming Magazine Website" 
+                  className="img-fluid"
+                />
+                <Card.Body gap="2" className="text-center">
+                  <Card.Title>Gamming Magazine Website</Card.Title>
+                  <Card.Description>
+                    created using html,css,bootstrap,Gsap
+                  </Card.Description>
+                </Card.Body>
+                <Card.Footer gap="2"></Card.Footer>
+              </Card.Root>
+            </a>
+          </div>
+          
+          {/* second col */}
+          <div className="col-12 col-md-6 col-lg-4 mb-4">
+            <a href="https://github.com/Sreejith0098/zomato-clone.git">
+              <Card.Root overflow="hidden" className="h-100">
+                <Image 
+                  src={zomato} 
+                  alt="Zomato Clone" 
+                  className="img-fluid"
+                />
+                <Card.Body gap="2" className="text-center">
+                  <Card.Title>Zomato Clone</Card.Title>
+                  <Card.Description>
+                    Zomato clone using html,css,bootstrap
+                  </Card.Description>
+                </Card.Body>
+                <Card.Footer gap="2"></Card.Footer>
+              </Card.Root>
+            </a>
+          </div>
+          
+          {/* third col */}
+          <div className="col-12 col-md-6 col-lg-4 mb-4">
+            <a href="https://github.com/Sreejith0098/trends2.git">
+              <Card.Root overflow="hidden" className="h-100">
+                <Image 
+                  src={trends} 
+                  alt="Trends Clone" 
+                  className="img-fluid"
+                />
+                <Card.Body gap="2" className="text-center">
+                  <Card.Title>Trends Clone</Card.Title>
+                  <Card.Description>
+                    Trends clone using html,css,bootstrap
+                  </Card.Description>
+                </Card.Body>
+                <Card.Footer gap="2"></Card.Footer>
+              </Card.Root>
+            </a>
+          </div>
         </div>
       </div>
     </div>
